828a2f40b78070c908c3fff799b5ba931f7602dc
niceman1994 <niceman1994@naver.com>
2022-09-02 12:55:22 +0900

    Unity 2022/09/02-1

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -28,17 +28,17 @@ public class FollowCamera : MonoBehaviour
     {
         // 측면으로 돌릴때 부드럽게 이동하도록 코드 작성하기
 
-        // 부드러운 이동
-        //transform.position = Vector3.Lerp(
-        //    transform.position,
-        //    Target.position + Offset,
-        //    Time.deltaTime * 2.0f);
+        //부드러운 이동
+        transform.position = Vector3.Lerp(
+            transform.position,
+            Target.position + Offset,
+            Time.deltaTime * 2.0f);
 
-        transform.LookAt(Target);
+        //transform.LookAt(Target);
 
         // 부드러운 회전
         Vector3 CameraAngles = transform.rotation.eulerAngles;
-        CameraAngles.y = Input.GetAxis("Mouse X") * 10.0f;
+        CameraAngles.y += Input.GetAxis("Mouse X") * 10.0f;
 
         Quaternion CameraQuaternion = Quaternion.Euler(CameraAngles);
 
@@ -47,23 +47,25 @@ public class FollowCamera : MonoBehaviour
             CameraQuaternion,
             Time.deltaTime * 10.0f);
 
-  //      if (Input.GetMouseButtonDown(1))
-  //      {
-  //          StartPos = Input.mousePosition;
-  //      }
+        //transform.RotateAround(Target.position, Vector3.up, CameraQuaternion.y);
+        //transform.LookAt(Target);
 
-  //      if (Input.GetMouseButton(1))
-  //      {
-  //          EndPos = Input.mousePosition;
-  //      }
-
-  //      if (Input.GetMouseButtonUp(1))
-  //      {
-  //          EndPos = Input.mousePosition;
-
-  //          StartPos = new Vector3(0.0f, 0.0f, 0.0f);
-  //          EndPos = new Vector3(0.0f, 0.0f, 0.0f);
-  //      }
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    StartPos = Input.mousePosition;
+        //
+        //if (Input.GetMouseButton(1))
+        //{
+        //    EndPos = Input.mousePosition;
+        //}
+        //
+        //if (Input.GetMouseButtonUp(1))
+        //{
+        //    EndPos = Input.mousePosition;
+        //
+        //    StartPos = new Vector3(0.0f, 0.0f, 0.0f);
+        //    EndPos = new Vector3(0.0f, 0.0f, 0.0f);
+        //}
 
         Debug.Log(StartPos + " , " + EndPos);
     }
