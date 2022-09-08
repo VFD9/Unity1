9eb80270b22eb25e9a73ace3c5937141285b738d
niceman1994 <niceman1994@naver.com>
2022-09-08 13:07:34 +0900

    Unity 2022/09/08-1

--- Assets/Resources/Scripts/Point.cs
+++ Assets/Resources/Scripts/Point.cs
@@ -5,35 +5,11 @@ using UnityEngine;
 public class Point : MonoBehaviour
 {
     [SerializeField] private float fTime;
-    //[SerializeField] private GameObject Enemy;
-    //[SerializeField] private GameObject EnemyObj;
     [SerializeField] private EnemyController Obj;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, 1.0f);
-    }
-
-    void Start()
-    {
-        //fTime = Random.Range(5.0f, 10.0f);
-        fTime = 0;
-    }
-    
-    void Update()
-    {
-        //fTime -= Time.deltaTime;
-
-        if(fTime <= 0)
-        {
-            fTime = Random.Range(5.0f, 10.0f);
-
-            // Enemy »ý¼º
-            EnemyController cObj = Instantiate(Obj);
-            cObj.transform.parent = transform;
-            
-            cObj.transform.position = transform.position;
-        }
     }
 }
